05fee7ce49387040a12ceb979b71b78313578253
Noah Amoo <yawnoah3n@yahoo.com>
2026-06-25 23:43:59 +0100

    Added logic for the leftout operators

--- Python_Fundamentals_I/simple_calculator.py
+++ Python_Fundamentals_I/simple_calculator.py
@@ -8,9 +8,20 @@ def calculate(num1, num2, operator):
     if operator == '*':
         return num1 * num2
     
+    if operator == '**':
+        return num1 ** num2
+    
+    if operator == '%':
+        return num1 % num2
+    
     if operator == '/':
         if num2 != 0:
             return num1 / num2
+        return ZeroDivisionError
+    
+    if operator == '//':
+        if num2 != 0:
+            return num1 // num2
         return ZeroDivisionError
     
     # If the user enters a letter for a number or the wrong operator,
@@ -20,6 +31,6 @@ def calculate(num1, num2, operator):
 
 fnum = float(input("Enter your first number: "))
 snum = float(input("Enter your second number: "))
-operator = input("Enter one of these opereators: +, -, * or /: ")
+operator = input("Enter one of these opereators: +, -, *, **, %, // or /: ")
 
 print(calculate(fnum, snum, operator))
