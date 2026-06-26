6d5b9dd8410a84edadd3edb636574d7eb81f456d
Noah Amoo <yawnoah3n@yahoo.com>
2026-06-26 12:19:29 +0100

    Completed second part of the assignment

--- Python_Fundamentals_I/3_string_manipulations.py
+++ Python_Fundamentals_I/3_string_manipulations.py
@@ -1,3 +1,5 @@
+# 1) Write a function that takes a sentence and returns: word count, character count (excluding spaces), and the longest word.
+
 def sentence_analysis(sentence):
     words = sentence.split()
     word_count = len(words)
@@ -11,3 +13,17 @@ def sentence_analysis(sentence):
 info = "I am the best programmer in the world"
 
 print(sentence_analysis(info))
+
+
+#2) Create a password validator that checks: minimum 8 characters, at least one uppercase, one lowercase, one digit, and one special character. Use string methods only (no regex).
+
+def password_validator(password):
+    has_min_char = len(password) >= 8
+    has_uppercase = any(char.isupper() for char in password)
+    has_lowercase = any(char.islower() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+    has_special = any(not char.isalnum() for char in password)
+
+    return has_min_char and has_uppercase and has_lowercase and has_digit and has_special
+
+print(password_validator("Noa3h@Amoo")) # Returns True
